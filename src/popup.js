f5dd9d8b6c429f2af92295ab47d8b2a85034eb84
germaniuz <germanius@list.ru>
2016-07-18 01:26:14 +0300

    Add two popboxes in demo and create variables for some tune plugin

--- src/popup.js
+++ src/popup.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
 
+  var targetAttr = "data-popup-target";
+  var activeClass = "pop-active";
   var target = "";
   var targetBox = "";
   var activeBox = "";
 
   function openBox(){
-    target = $(this).attr("data-target");
-    targetBox = '.pop-box[data-target = ' + target + ']';
+    target = $(this).attr(targetAttr);
+    targetBox = '.pop-box[' + targetAttr + '=' + target + ']';
     $(".pop-bg").fadeIn(300);
-    $(targetBox).fadeIn(600).addClass('pop-active');
+    $(targetBox).fadeIn(600).addClass(activeClass);
     return false;
   };
 
   function closeBox(){
-    activeBox = $('.pop-box.pop-active');
+    activeBox = $('.pop-box.' + activeClass);
     $('.pop-bg').fadeOut(600);
     $(activeBox).fadeOut(300);
   }
