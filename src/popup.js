2222a039deb01509f84d08524e67c6bb85f45c6b
germaniuz <germanius@list.ru>
2016-07-18 01:17:49 +0300

    Add closeBox function

--- src/popup.js
+++ src/popup.js
@@ -2,14 +2,22 @@ $(document).ready(function(){
 
   var target = "";
   var targetBox = "";
+  var activeBox = "";
 
   function openBox(){
     target = $(this).attr("data-target");
     targetBox = '.pop-box[data-target = ' + target + ']';
     $(".pop-bg").fadeIn(300);
-    $(targetBox).fadeIn(500).addClass('pop-active');
+    $(targetBox).fadeIn(600).addClass('pop-active');
     return false;
   };
 
+  function closeBox(){
+    activeBox = $('.pop-box.pop-active');
+    $('.pop-bg').fadeOut(600);
+    $(activeBox).fadeOut(300);
+  }
+
   $('a').click(openBox);
+  $('.pop-close-btn, .pop-bg').click(closeBox);
 });
